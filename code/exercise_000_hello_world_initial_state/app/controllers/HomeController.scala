f7b34bc9c5faa8a0ed0c6f9568a49c3fa6452322
Sacha Garnier <44492283+sachgar@users.noreply.github.com>
2023-04-11 09:52:34 +0200

    Example simplified

--- code/exercise_000_hello_world_initial_state/app/controllers/HomeController.scala
+++ code/exercise_000_hello_world_initial_state/app/controllers/HomeController.scala
@@ -22,12 +22,4 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     Ok(views.html.index())
   }
   
-  def explore() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.explore())
-  }
-  
-  def tutorial() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.tutorial())
-  }
-  
 }
