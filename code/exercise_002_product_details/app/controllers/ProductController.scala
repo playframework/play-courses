84a10c2372395d1662c915ab705f92c5b303a449
Sacha Garnier <44492283+sachgar@users.noreply.github.com>
2023-04-25 10:08:16 +0200

    Exercise 002 done

---
+++ code/exercise_002_product_details/app/controllers/ProductController.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import play.api.i18n._
+import javax.inject.Inject
+import play.api.mvc.{Action, AbstractController, ControllerComponents, Request, AnyContent}
+import models.Product
+
+class ProductController @Inject() (cc: ControllerComponents) extends AbstractController(cc) with I18nSupport {
+  def list = Action { implicit request: Request[AnyContent] =>
+    val products = Product.findAll
+
+    Ok(views.html.list(products))
+  }
+
+  def show(ean: Long) = Action { implicit request: Request[AnyContent] =>
+    Product.findByEan(ean).map(product =>
+      Ok(views.html.products.details(product))
+    ).getOrElse(NotFound)  
+  }
+}
---
+++ code/exercise_002_product_details/conf/messages.es
@@ -0,0 +1,8 @@
+# https://www.playframework.com/documentation/latest/ScalaI18N
+application.name = Catalogo de productos
+
+ean = EAN
+name = Nombre
+description = Descripción
+
+product.details = Producot: {0}
