7c001df36c3b083a1f2f3b9bb435bc2476bab24d
Sacha Garnier <44492283+sachgar@users.noreply.github.com>
2023-04-25 10:27:39 +0200

    exercise_001 fixed

--- code/exercise_001_products_list_page/app/controllers/ProductController.scala
+++ code/exercise_001_products_list_page/app/controllers/ProductController.scala
@@ -1,10 +1,11 @@
 package controllers
 
+import play.api.i18n._
 import javax.inject.Inject
 import play.api.mvc.{Action, AbstractController, ControllerComponents, Request, AnyContent}
 import models.Product
 
-class ProductController @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
+class ProductController @Inject() (cc: ControllerComponents) extends AbstractController(cc) with I18nSupport {
   def list = Action { implicit request: Request[AnyContent] =>
     val products = Product.findAll
 
